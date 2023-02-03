210703223b842d449795282cca5db746d3f01302
kudinroman <kudinroman82@gmail.com>
2023-02-03 12:52:39 +0300

    Add user roles

--- Makefile
+++ Makefile
@@ -4,4 +4,5 @@ css:
 	npx tailwindcss -i app/javascript/styles/tailwind_input.scss -o app/javascript/styles/tailwind_output.scss  -c ./tailwind.config.js -w
 console:
 	bundle exec rails c
-
+dbreset:
+	bundle exec rails db:drop && bundle exec rails db:create && bundle exec rails db:migrate && bundle exec rails db:seed
--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_30_145005) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
